3c87935aa96d3e8b201f54ac2c05c3d344661166
hailegnaw27 <ixvos406@gmail.com>
2023-12-23 05:12:57 -0800

    1-print_square.js

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+
 /*
     Print a square with the character #
     
@@ -7,19 +8,18 @@
 */
 
 if (process.argv.length <= 2) {
-    process.stdout.write("Missing argument\n");
-    process.stdout.write("Usage: ./1-print_square.js <size>\n");
-    process.stdout.write("Example: ./1-print_square.js 8\n");
+    process.stderr.write("Missing argument\n");
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
     process.exit(1);
 }
 
-const size = parseInt(process.argv[2]);
+let size = parseInt(process.argv[2], 10);
 
 for (let i = 0; i < size; i++) {
-    let row = "";
     for (let j = 0; j < size; j++) {
-        row += "#";
+        process.stdout.write("#");
     }
-    process.stdout.write(row + "\n");
+    process.stdout.write("\n");
 }
 
